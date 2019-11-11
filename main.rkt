04f371b5874bbf5bb98df85fd0755ac190a1f2a3
sonnynajar <sonny.najar@gmail.com>
2019-11-11 08:49:46 -0800

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -27,48 +27,54 @@
      "https://github.com/thoughtstem/ratchet.git#master"
      "https://github.com/thoughtstem/racket-chipmunk.git#4a0198a"
      "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util"
+     
      "https://github.com/thoughtstem/game-engine.git#master"
      "https://github.com/thoughtstem/game-engine-rpg.git#master"
      "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena-avengers#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena-fortnite#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena-starwars#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=fundamentals#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=k2#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=survival#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=survival-minecraft#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=survival-pokemon#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=adventure#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=adventure-harrypotter#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=adventure-mario#master"
+     
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=battlearena#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=battlearena-avengers#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=battlearena-fortnite#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=battlearena-starwars#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=survival#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=survival-minecraft#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=survival-pokemon#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=adventure#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=adventure-harrypotter#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=adventure-mario#master"
 
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=k2#master"
 
      "https://github.com/thoughtstem/TS-K2-Languages.git?path=clicker-cartoon-collect#master"
      "https://github.com/thoughtstem/TS-K2-Languages.git?path=clicker-cartoon-avoid#master"
      "https://github.com/thoughtstem/TS-K2-Languages.git?path=clicker-cartoon-special#master"
+     
+     "https://github.com/thoughtstem/TS-K2-Languages.git?path=healer-animal-foods#master"
+     "https://github.com/thoughtstem/TS-K2-Languages.git?path=healer-animal-friends#master"
+     "https://github.com/thoughtstem/TS-K2-Languages.git?path=healer-animal-enemies#master"
 
      "https://github.com/thoughtstem/TS-VR-Languages.git?path=3d-exploration#master"
      
-     "https://github.com/thoughtstem/TS-GE-Languages.git?path=data-sci#master"
+     "https://github.com/thoughtstem/TS-MISC-Languages.git?path=data-sci#master"
+     "https://github.com/thoughtstem/TS-MISC-Languages.git?path=fundamentals#master"
      )
   
   (displayln "BUILDING DOCS FOR STUDENT LANGUAGES")
   (fast-setup #:slow-mode? #t ;Builds the docs, which is not the default.
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena-avengers#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena-fortnite#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=battlearena-starwars#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=fundamentals#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=k2#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=survival#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=survival-minecraft#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=survival-pokemon#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=adventure#master"         
-     "https://github.com/thoughtstem/TS-Languages.git?path=adventure-harrypotter#master"
-     "https://github.com/thoughtstem/TS-Languages.git?path=adventure-mario#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=battlearena-avengers#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=battlearena-fortnite#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=battlearena-starwars#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=survival#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=survival-minecraft#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=survival-pokemon#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=adventure#master"         
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=adventure-harrypotter#master"
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=adventure-mario#master"
      
-     "https://github.com/thoughtstem/TS-GE-Languages.git?path=data-sci#master"
+     "https://github.com/thoughtstem/TS-MISC-Languages.git?path=data-sci#master"
+     "https://github.com/thoughtstem/TS-MISC-Languages.git?path=fundamentals#master"
+     
+     "https://github.com/thoughtstem/TS-GE-Languages.git?path=k2#master"
      ) 
         
 )
@@ -86,6 +92,7 @@
   (watch! 'ratchet)
   (watch! 'racket-chipmunk)
   (watch! 'ts-kata-util)
+  
   (watch! 'game-engine)
   (watch! 'game-engine-rpg)
   (watch! 'game-engine-demos-common)
@@ -94,8 +101,6 @@
   (watch! 'battlearena-fortnite)
   (watch! 'battlearena-starwars)
   (watch! 'battlearena)
-  (watch! 'fundamentals)
-  (watch! 'k2)
   (watch! 'survival-minecraft)
   (watch! 'survival-pokemon)
   (watch! 'survival)
@@ -103,7 +108,22 @@
   (watch! 'adventure-mario)
   (watch! 'adventure)
   
+  (watch! 'k2) ;remove after Fall 2019
+  
   (watch! 'data-sci)
+  (watch! 'fundamentals)
+  
+  (watch! 'clicker-cartoon-collect)
+  (watch! 'clicker-cartoon-avoid)
+  (watch! 'clicker-cartoon-special)
+     
+  (watch! 'healer-animal-foods)
+  (watch! 'healer-animal-friends)
+  (watch! 'healer-animal-enemies)
+
+  (watch! '3d-exploration)
+  
+  
   )
 
 
